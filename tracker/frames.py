9210f623dc79b1de7c259339a1e23bb5c18c6f2f
win <samuli.siukonen@gmail.com>
2025-02-12 21:29:24 +0200

    add frames.py to get coordinates from frames

---
+++ tracker/frames.py
@@ -0,0 +1,65 @@
+from ultralytics import YOLO
+import cv2
+
+def load_video(video_filepath:str):
+    """
+    Parameters
+    ----------
+    video_filepath : str
+        Relative path to videofile.
+
+    Returns
+    -------
+    cv2.VideoCapture object
+    """
+    vcap = cv2.VideoCapture(video_filepath)
+    if vcap.isOpened():
+        print("frame count:", vcap.get(cv2.CAP_PROP_FRAME_COUNT))
+        print("frame width:", vcap.get(cv2.CAP_PROP_FRAME_WIDTH))
+        print("frame height:", vcap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        print("fps:", vcap.get(cv2.CAP_PROP_FPS))
+    cv2.destroyAllWindows()
+    return vcap
+
+def read_video(video_capture:cv2.VideoCapture, model_path:str):
+    """
+    Parameters
+    ----------
+    video_capture : cv2.VideoCapture object
+    
+    model : str
+        Relative path to YOLO model.
+
+    Returns
+    -------
+    Generator of Results objects.
+    """
+    model = YOLO(model_path)
+
+    while True:
+        retval, frame = video_capture.read() # Read one frame from video
+        if not retval:
+            break
+        results = model(frame, stream=True) # Run YOLO detectino on frame
+        
+        for result in results:
+            class_names = result.names # class names
+            boxes = {}
+            
+            # iterate over each box
+            for i, box in enumerate(result.boxes):
+                x1, y1, x2, y2 = box.xyxy[0] # get coordinates
+                x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2) # convert from tensor to int
+                boxes[i] = {"class":class_names[int(box.cls)],
+                            "confidence":float(box.conf),
+                            "x1":x1,
+                            "y1":y1,
+                            "x2":x2,
+                            "y2":y2}
+                
+            yield boxes
+                
+    
+
+
+
