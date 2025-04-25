7371e50b7a8ab7b208fb56dba026f2c50a23eacb
win <samuli.siukonen@gmail.com>
2025-04-25 16:50:10 +0300

    add treshold to stick detection

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from utils.features import extract_features
 
 
 
-json_filepath = 'data/raw_data/CAGE_030720_HA70345_exploration_IB.json'
+json_filepath = 'data/raw_data/CAGE_200520_HA70336_exploration_IB.json'
 data_raw = load_json_to_dict(json_filepath)
 video_filename = data_raw["video_filename"]
 frame_count = int(data_raw["frame_count"])
@@ -11,7 +11,7 @@ fps = int(data_raw["fps"])
 
 results_df, quality_metrics = extract_features(
     data_raw,
-    window_size_mean=3,
+    window_size_mean=5,
     window_size_mode=31,
     fps=fps,
     frame_count=frame_count,
@@ -19,3 +19,7 @@ results_df, quality_metrics = extract_features(
 
 print(quality_metrics.head())
 print(results_df.head())
+
+
+
+
