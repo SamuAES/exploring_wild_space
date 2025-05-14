c3e3129834973d86d07b08a5d99b7cb4a63ef5c7
Tatu Linnala <tatu.linnala@gmail.com>
2025-05-14 18:46:19 +0300

    Update window_size_mode parameter

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 from utils.frames import load_json_to_dict
 from utils.features import extract_features
 
-
-
 json_filepath = 'data/raw_data/CAGE_200520_HA70336_exploration_IB.json'
 data_raw = load_json_to_dict(json_filepath)
 video_filename = data_raw["video_filename"]
@@ -12,7 +10,7 @@ fps = int(data_raw["fps"])
 results_df, quality_metrics = extract_features(
     data_raw,
     window_size_mean=5,
-    window_size_mode=31,
+    window_size_mode=35,
     fps=fps,
     frame_count=frame_count,
 )
